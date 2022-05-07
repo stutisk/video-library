345dda1598294e69b5a8cb4663bae472ad93e9a6
stutisk <48529714+stutisk@users.noreply.github.com>
2022-05-08 02:34:58 +0530

    initial tailwind setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import MockAPI  from "./components/Mockman/Mockman";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
     return (
       <div className="App">
       <Routes>    
-        <Route path="/mock" element={<MockAPI/>} />        
+        <Route path="/mock" element={<MockAPI/>} />      
       </Routes> 
       </div>
     );
