f20967c505efb80388e8f7259af6dfbeb384b5e9
stutisk <48529714+stutisk@users.noreply.github.com>
2022-05-11 14:22:34 +0530

    feat:minor changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import MockAPI  from "./components/Mockman/Mockman";
 import { Routes, Route } from "react-router-dom";
 import { Homepage } from "./Pages/Homepage/Homepage";
+import { Sidebar } from "./components/Sidebar/Sidebar";
 
 function App() {
     return (
-      <div className="App">
-      <Routes>  
+      <div className="App">  
+      <main className="h-screen">
+      <div className="flex flex-row h-screen">
+        <div className="basis-72  bg-slate-800  ">
+          <Sidebar />
+        </div>
+        <div className="basis-full bg-slate-900">
+        <Routes> 
       <Route path="/" element={<Homepage/>} />    
-        <Route path="/mock" element={<MockAPI/>} />      
+      <Route path="/mock" element={<MockAPI/>} />      
       </Routes> 
+        </div>
+      </div>
+    </main>
+    
       </div>
     );
   }
