596879b77ebb60d73247150aa166c973d5c7eff6
stutisk <48529714+stutisk@users.noreply.github.com>
2022-05-12 14:42:05 +0530

    feat:added categories backend

--- src/backend/db/categories.js
+++ src/backend/db/categories.js
@@ -8,20 +8,38 @@ import { v4 as uuid } from "uuid";
 export const categories = [
   {
     _id: uuid(),
-    categoryName: "Computer Programming",
-    description:
-      "Computer programming is the process of designing and building an executable computer program to accomplish a specific computing",
+    categoryName: "Chill",
+    image:
+      "https://res.cloudinary.com/dvbw8xujd/image/upload/v1652301557/chill_izhoq0.jpg",
   },
   {
     _id: uuid(),
-    categoryName: "Frontend Development",
-    description:
-      "Front-end web development, also known as client-side development is the practice of producing HTML, CSS and JavaScript for a website or Web Application",
+    categoryName: "Workout",
+    image:
+      "https://res.cloudinary.com/dvbw8xujd/image/upload/v1652301558/Workout_srhapr.jpg",
   },
   {
     _id: uuid(),
-    categoryName: "Backend Development",
-    description:
-      "Backend Development is also known as server-side development. It is everything that the users don't see and contains behind-the-scenes activities that occur when performing any action on a website.",
+    categoryName: "Focus",
+    image:
+    "https://res.cloudinary.com/dvbw8xujd/image/upload/v1652301558/Focus_wxddfh.jpg",
+  },
+  {
+    _id: uuid(),
+    categoryName: "Feel Good",
+    image:
+      "https://res.cloudinary.com/dvbw8xujd/image/upload/v1652301558/feel_good_zjshk5.jpg",
+  },
+  {
+    _id: uuid(),
+    categoryName: "Sleep",
+    image:
+      "https://res.cloudinary.com/dvbw8xujd/image/upload/v1652301557/sleep_t5atuk.jpg",
+  },
+  {
+    _id: uuid(),
+    categoryName: "Party",
+    image:
+    "https://res.cloudinary.com/dvbw8xujd/image/upload/v1652301558/party_z6h0by.jpg",
   },
 ];
