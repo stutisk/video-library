ccc92038686aef6afbcd47c2ef63438c34e1e84e
stutisk <48529714+stutisk@users.noreply.github.com>
2022-05-12 14:47:16 +0530

    added pages in ones file

---
+++ src/Pages/index.js
@@ -0,0 +1,9 @@
+import { Historypage } from "./Historypage/Historypage";
+import {Homepage } from "./Homepage/Homepage";
+import {Likedvideos} from "./Likedvideos/Likedvideos";
+import {Playlist} from "./Playlist/Playlist";
+import {Videolistingpage} from "./Videolistingpage/Videolistingpage";
+import { Watchlater } from "./Watchlater/Watchlater";
+
+export{Historypage,Homepage,Likedvideos, Playlist,Videolistingpage,Watchlater}
+
