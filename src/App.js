93a04ed2c529268e7ec4eed3b8ea2c0944b314c6
stutisk <48529714+stutisk@users.noreply.github.com>
2022-05-12 14:45:54 +0530

    feat:added routes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,40 @@
-import MockAPI  from "./components/Mockman/Mockman";
+import MockAPI from "./components/Mockman/Mockman";
 import { Routes, Route } from "react-router-dom";
-import { Homepage } from "./Pages/Homepage/Homepage";
 import { Sidebar } from "./components/Sidebar/Sidebar";
+import { Navbar } from "./components/Navbar/Navbar";
+import {Historypage,Homepage,Likedvideos, Playlist,Videolistingpage,Watchlater } from "./Pages/index"
 
 function App() {
-    return (
-      <div className="App">  
-      <main className="h-screen">
-      <div className="flex flex-row h-screen">
-        <div className="basis-72  bg-slate-800  ">
-          <Sidebar />
+  return (
+    <div className="App">
+      <main className="">
+        <div className="flex flex-row  ">
+          <div className="basis-72    ">
+            <Sidebar />
+          </div>
+          <div className="basis-full ">
+            <div className="flex flex-col ">
+              <div className="basis-20  p-4  ">
+                <Navbar />{" "}
+              </div>
+              <div className="basis-full  text-2xl text-slate-50 text-center ">
+                <Routes>
+                  <Route path="/" element={<Homepage />} />
+                  <Route path="/mock" element={<MockAPI />} />
+                  <Route path="/history" element={<Historypage />} />
+                  <Route path="/likedVideos" element={<Likedvideos />} />
+                  <Route path="/playlist" element={<Playlist />} />
+                  <Route path="/videoList" element={<Videolistingpage />} />
+                  <Route path="/watchLater" element={<Watchlater />} />
+
+                </Routes>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="basis-full bg-slate-900">
-        <Routes> 
-      <Route path="/" element={<Homepage/>} />    
-      <Route path="/mock" element={<MockAPI/>} />      
-      </Routes> 
-        </div>
-      </div>
-    </main>
-    
-      </div>
-    );
-  }
-  
-  export default App;
+      </main>
+    </div>
+  );
+}
+
+export default App;
