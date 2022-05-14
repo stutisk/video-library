8464e56ffce3886676a9c4339b9b81226ef89580
stutisk <48529714+stutisk@users.noreply.github.com>
2022-05-14 22:31:51 +0530

    feat:added categories via featch call

--- src/components/Cards/Categorycard.jsx
+++ src/components/Cards/Categorycard.jsx
@@ -1,83 +1,47 @@
+import react ,{useState,useEffect} from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
 const Categorycard = () => {
+  const [categories, setCategories] = useState([]);
+
+  async  function getCategories  () {
+    try {
+      const res = await axios.get("/api/categories");
+      console.log(res.data)
+      setCategories(res.data.categories);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+      getCategories();
+  }, []);
+
   return (
+
     <div className="flex flex-row flex-wrap  gap-10 p-4 justify-center ">
-      <div className=" border-2 hover:bottom-4 border-sky-500  shadow hover:shadow-md  shadow-sky-500  hover:shadow-sky-600 rounded cursor-pointer ">
+      { categories.map((category) => {
+      return(
+        <Link to="">
+        <div className=" border-2 hover:bottom-4 border-sky-500  shadow hover:shadow-md  shadow-sky-500  hover:shadow-sky-600 rounded cursor-pointer ">
         <div>
           <img
             className="h-auto w-auto hover:opacity-50 "
-            src="https://res.cloudinary.com/dvbw8xujd/image/upload/c_scale,h_250/v1652301557/chill_izhoq0.jpg"
+            src={category.image}
           />
         </div>
-        {/* <div className="absolute   inset-2 ">listen tro music and chill </div> */}
+       
         <p className="p-2 tracking-wide" >
-           Chill
+          {category.categoryName}
         </p>
       </div>
-      
-      <div className=" border-2 hover:bottom-4 border-sky-500  shadow hover:shadow-md  shadow-sky-500  hover:shadow-sky-600 rounded cursor-pointer ">
-        <div>
-          <img
-            className="h-auto w-auto hover:opacity-50 "
-            src="https://res.cloudinary.com/dvbw8xujd/image/upload/c_scale,h_250/v1652301557/chill_izhoq0.jpg"
-          />
-        </div>
-        {/* <div className="absolute   inset-2 ">listen tro music and chill </div> */}
-        <div className="p-2 tracking-wide" >
-            Chill
-        </div>
-      </div>
-      <div className=" border-2 hover:bottom-4 border-sky-500  shadow hover:shadow-md  shadow-sky-500  hover:shadow-sky-600 rounded cursor-pointer ">
-        <div>
-          <img
-            className="h-auto w-auto hover:opacity-50 "
-            src="https://res.cloudinary.com/dvbw8xujd/image/upload/c_scale,h_250/v1652301557/chill_izhoq0.jpg"
-          />
-        </div>
-        {/* <div className="absolute   inset-2 ">listen tro music and chill </div> */}
-        <div className="p-2 tracking-wide" >
-            Chill
-        </div>
-      </div>
-
-      <div className=" border-2 hover:bottom-4 border-sky-500  shadow hover:shadow-md  shadow-sky-500  hover:shadow-sky-600 rounded cursor-pointer ">
-        <div>
-          <img
-            className="h-auto w-auto hover:opacity-50 "
-            src="https://res.cloudinary.com/dvbw8xujd/image/upload/c_scale,h_250/v1652301557/chill_izhoq0.jpg"
-          />
-        </div>
-        {/* <div className="absolute   inset-2 ">listen tro music and chill </div> */}
-        <div className="p-2 tracking-wide" >
-            Chill
-        </div>
-      </div>
-
-      <div className=" border-2 hover:bottom-4 border-sky-500  shadow hover:shadow-md  shadow-sky-500  hover:shadow-sky-600 rounded cursor-pointer ">
-        <div>
-          <img
-            className="h-auto w-auto hover:opacity-50 "
-            src="https://res.cloudinary.com/dvbw8xujd/image/upload/c_scale,h_250/v1652301557/chill_izhoq0.jpg"
-          />
-        </div>
-        {/* <div className="absolute   inset-2 ">listen tro music and chill </div> */}
-        <div className="p-2 tracking-wide" >
-            Chill
-        </div>
-      </div>
-      <div className=" border-2 hover:bottom-4 border-sky-500  shadow hover:shadow-md  shadow-sky-500  hover:shadow-sky-600 rounded cursor-pointer ">
-        <div>
-          <img
-            className="h-auto w-auto hover:opacity-50 "
-            src="https://res.cloudinary.com/dvbw8xujd/image/upload/c_scale,h_250/v1652301557/chill_izhoq0.jpg"
-          />
-        </div>
-        {/* <div className="absolute   inset-2 ">listen tro music and chill </div> */}
-        <div className="p-2 tracking-wide" >
-            Chill
-        </div>
-      </div>
-
+      </Link>
+      )
+       
      
+       } )}
     </div>
   );
 };
