32862b22972d2bd6388c684787250ad2dfebe1d0
stutisk <48529714+stutisk@users.noreply.github.com>
2022-05-17 12:03:15 +0530

    feat:login functionality

--- src/Pages/Authpages/Login.jsx
+++ src/Pages/Authpages/Login.jsx
@@ -1,7 +1,61 @@
 import {IoIosArrowDropright} from "../../components/Icons/Icons"
 import { Link } from "react-router-dom";
+import { useRef,useEffect,useState } from "react";
+import { useAuth } from "../../Context/AuthContext";
+import { useNavigate, useLocation } from "react-router-dom";
+import axios from "axios";
+
 const Login = () => {
+  const useremail = useRef();
+  const userpass = useRef();
+  const navigate = useNavigate();
+  const location = useLocation();
+
+ const {isLogin,setLogin,Error, setError}=useAuth();
+ 
+
+ const loginHandler=async()=>{
+  try {
+    const response = await axios.post(`/api/auth/login`, {
+      email:useremail.current.value,
+      password:userpass.current.value
+    });
+    setLogin(true);
+    console.log(response)
+    console.log(response.data.error)
+    const userDetail = {
+      Email: response.data.foundUser.email,
+      name:
+        response.data.foundUser.firstName +
+        " " +
+        response.data.foundUser.lastName,
+    };
+    localStorage.setItem("token", response.data.encodedToken);
+    localStorage.setItem("user", JSON.stringify(userDetail));
+  } 
+  catch (error) {
+    console.log( error.response.data.errors[0] )
+    setError({  error: error.response.data.errors[0] });
+   }
+ 
+ }
+  const guestLogin = () => {
+    useremail.current.value = "adarshbalika@gmail.com";
+    userpass.current.value="adarshBalika123"
+loginHandler();
+    
+    console.log(useremail.current.value);
+    
+  };
+  useEffect(() => {
+    if (isLogin && location?.state?.from?.pathname) {
+      navigate(location.state.from.pathname);
+    } else if (isLogin) {
+      navigate("/");
+    }
+  }, [isLogin]);
   return (
+   
       <section className="pl-60 pt-5">
     <div class="w-full max-w-sm ">
       <form class="  border-2 border-sky-500 shadow shadow-sky-500 rounded px-3 pt-6 pb-8 mb-4 ">
@@ -9,6 +63,7 @@ const Login = () => {
           <label
             class="block text-slate-50 text-xl  mb-2"
             for="username"
+            
           >
            Email Address
           </label>
@@ -17,6 +72,8 @@ const Login = () => {
             id="username"
             type="text"
             placeholder="stuti12345@gmail.com"
+            ref={useremail} 
+           
           />
         </div>
         <div class="mb-6">
@@ -31,19 +88,24 @@ const Login = () => {
             id="password"
             type="password"
             placeholder="**********"
+            ref={userpass}
           />
         </div>
+      
+        
         <div class="flex items-center justify-between p-3 gap-3">
         <button
               type="button"
               class="  inline-block px-6 py-2.5   text-slate-50 font-medium text-xl  bg-sky-500 rounded hover:bg-sky-600  focus:ring-0 transition duration-150 ease-in-out"
+            onClick={loginHandler}
             >
             LOGIN
         </button>
         <button
               type="button"
               class="  inline-block px-6 py-2.5   text-slate-50 font-medium text-xl  border-2 hover:border-2 border-sky-500 hover:border-double    rounded   focus:ring-0 transition duration-150 ease-in-out"
-            >
+            
+      onClick={guestLogin}>
             LOGIN AS GUEST
         </button>
          
@@ -63,10 +125,11 @@ const Login = () => {
           >
             Create New Account
             <IoIosArrowDropright size={26}  />
-          </Link>
-         
-          
+          </Link>        
         </div>
+        <div  class="inline-block   text-base text-red-700  p-2">
+        {Error.error}       
+        </div>   
       </form>
     </div>
     </section>
