2b535278ca04b912e26e541da213b93b64ba8a8b
stutisk <48529714+stutisk@users.noreply.github.com>
2022-05-17 12:02:52 +0530

    feat:updated user

--- src/backend/db/users.js
+++ src/backend/db/users.js
@@ -9,10 +9,10 @@ import { formatDate } from "../utils/authUtils";
 export const users = [
   {
     _id: uuid(),
-    firstName: "Stuti",
-    lastName: "SK",
-    email: "Stutisk@gmail.com",
-    password: "Stutisk123",
+    firstName: "Adarsh",
+    lastName: "Balika",
+    email: "adarshbalika@gmail.com",
+    password: "adarshBalika123",
     createdAt: formatDate(),
     updatedAt: formatDate(),
   },
