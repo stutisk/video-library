9042d364d9d690da00a300528ee2a4cd3c4075c2
stutisk <48529714+stutisk@users.noreply.github.com>
2022-05-19 16:05:54 +0530

    feat:aded category and video cards

--- src/components/Cards/Videocards.jsx
+++ src/components/Cards/Videocards.jsx
@@ -1,46 +1,55 @@
-import {BiLike,MdWatchLater,MdOutlineFeaturedPlayList } from "../Icons/Icons"
+import {BiLike,MdWatchLater,MdOutlineFeaturedPlayList } from "../Icons/Icons";
+import { useData } from "../../Context/dataContext";
+import {getFilterVideos} from "../../utils/getFilterVideos";
 const Videocards = () => {
+
+  const {
+    state: { videos,categoryBy},
+   
+  } = useData();
+ 
+  const FinalProductList = getFilterVideos(categoryBy, videos);
   return (
-    
-      <div className="w-72 border-2 hover:bottom-4 border-sky-500  shadow hover:shadow-md  shadow-sky-500  hover:shadow-sky-600 rounded cursor-pointer ">
+    <div className="flex flex-row flex-wrap  gap-10 p-4 justify-center ">
+    {FinalProductList.map((videos) => {
+      return (
+        <div className="w-72 border-2 hover:bottom-4 border-sky-500  shadow hover:shadow-md  shadow-sky-500  hover:shadow-sky-600 rounded cursor-pointer ">
         <div>
           <img
             className="h-auto  "
-            src="https://res.cloudinary.com/dvbw8xujd/image/upload/v1652634336/Chill1_ehleua.webp"
+            src={videos.image}
+            alt={videos.categoryName}
           />
         </div>
         <div className="flex p-2 gap-3">
           <div>
             <img
               className="  rounded-full"
-              src="https://res.cloudinary.com/dvbw8xujd/image/upload/v1652634336/Chill_creator_qapxan.jpg"
+              src={videos.creator}
             />
           </div>
           <div>
             {" "}
             <p className=" tracking-wide text-base text-left">
-              Sad Songs | Hindi | Loneliness 
+             {videos.title}
             </p>
           </div>
           <div className="flex gap-2">
           <BiLike/>
           <MdWatchLater/>
           <MdOutlineFeaturedPlayList/>
-          </div>
-         
-          
-          
+          </div>    
         </div>
         <div className="flex place-content-between tracking-wide text-base">
-              <div>3M views</div>
-              <div>june 17, 2000</div>
+              <div>{videos.views}views</div>
+              <div>{videos.date}</div>
           </div>
         
       </div>
-
-     
-
-   
+       );
+     })}
+    
+    </div>
   );
 };
 
