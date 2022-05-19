e4779646ba4ec415e3ef70c297649d657ea32129
stutisk <48529714+stutisk@users.noreply.github.com>
2022-05-19 16:05:34 +0530

    feat:Api calls

---
+++ src/utils/serverRequests.jsx
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+     const getCategories = async (dispatch) => {
+        try {
+          const res = await axios.get("/api/categories");
+          console.log(res.data.categories)
+          dispatch({ type:"SUCCESS_CATEGORIES", payload: res.data.categories });
+        
+        } catch (error) {
+          console.log(error);
+        }
+      }
+      const getVideos = async (dispatch) => {
+        try {
+          const res = await axios.get("/api/videos");
+          console.log(res.data.videos)
+          dispatch({ type:"SUCCESS_VIDEOS", payload: res.data.videos });
+        
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    
+ 
+
+  export {getCategories,getVideos}
