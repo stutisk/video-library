ec091c715b6177bc440bf17736ed19a243ad2219
stutisk <48529714+stutisk@users.noreply.github.com>
2022-05-21 15:00:21 +0530

    netlify error

--- src/components/Icons/Icons.jsx
+++ src/components/Icons/Icons.jsx
@@ -1,4 +1,4 @@
-import {MdExplore,MdOutlineFeaturedPlayList,MdWatchLater,MdOutlineHistory,MdSearch,MdNotifications,MdOutlineLightMode} from "react-icons/md";
+import {MdExplore,MdOutlineFeaturedPlayList,MdWatchLater,MdOutlineHistory,MdSearch,MdNotifications} from "react-icons/md";
 import {BiLike} from "react-icons/bi";
 import{FiLogIn} from "react-icons/fi";
 import{IoIosArrowDropright} from "react-icons/io";
